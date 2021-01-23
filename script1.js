37ff1f80dbc2188924f33ac76ff20a01eb29fee1
Jagadishgollapalli <77227327+Jagadishgollapalli@users.noreply.github.com>
2021-01-23 08:21:24 +0530

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,38 @@
+var request=new XMLHttpRequest();
+request.open("GET","https://restcountries.eu/rest/v2/all",true);
+request.send();
+request.onload=function(){
+    var data=JSON.parse(this.response);
+    for(i=0;i<=data.length;i++){
+        try{
+            var lang=data[i].latlng;
+        var name=data[i].name;
+        open(name,...lang)
+        }
+        catch (e) {
+            console.log('Invalid co-ordinate data for country: ' + name + ' ' + e.message);
+        }
+    
+    }
+}
+
+var open = function (name, lat, lang) {
+	var request = new XMLHttpRequest();
+	var url =
+		'https://api.openweathermap.org/data/2.5/weather?lat=' +
+		lat +
+		'&lon=' +
+		lang +
+		'&appid=b2658b66e79ba602351e12efe0d9c1bd';
+
+	request.open('GET', url, true);
+	request.send();
+	request.onload = function () {
+		try {
+			var result = JSON.parse(this.response);
+			console.log(`${name} : ${result.main.temp}`);
+		} catch (e) {
+			console.log('undefined response' + name);
+		}
+	};
+};
